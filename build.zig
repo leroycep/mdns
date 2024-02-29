ab55ee5a1e3f72d872521b89fd28a771a1271e2e
geemili <opensource@geemili.xyz>
2024-02-29 14:29:46 -0700

    refactor: remove `static inline` to make usable by zig

--- build.zig
+++ build.zig
@@ -4,13 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const mdns = b.addStaticLibrary(.{
         .name = "mdns",
         .target = target,
         .optimize = optimize,
     });
-    lib.addCSourceFile(.{ .file = .{ .path = "mdns.c" } });
-    lib.installHeader("mdns.h", "mdns.h");
-    lib.linkLibC();
-    b.installArtifact(lib);
+    mdns.addCSourceFile(.{ .file = .{ .path = "mdns.c" } });
+    mdns.installHeader("mdns.h", "mdns.h");
+    mdns.linkLibC();
+    b.installArtifact(mdns);
+
+    const exe = b.addExecutable(.{
+        .name = "mdns-test",
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.addCSourceFile(.{ .file = .{ .path = "mdns-test.c" } });
+    exe.linkLibrary(mdns);
+    exe.linkLibC();
+    b.installArtifact(exe);
 }
